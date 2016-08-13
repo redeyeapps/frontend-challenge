d47dae7090c64e64a0263147fca0b20a7481f500
Xpndable <xpndable@outlook.com>
2016-08-13 10:20:08 +0000

    Style and layout scaffold, basic selection logic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 import { Players } from 'services/players';
 
 export class App {
-  players = Players;
+  static inject() { return [Players]; }
+  constructor(players) {
+    this.players = Players;
+    this.player1sel = false;
+    this.player2sel = false;
+    this.player1val = "";
+    this.player2val = "";
+  }
+  
+  play(player) {
+    if (!this.player1sel) {
+      console.log("Player 1 Selected");
+      this.player1val = player;
+      this.player1sel = true;
+    }
+    else if (!this.player2sel) {
+      console.log("Player 2 Selected");
+      this.player2val = player;
+      this.player2sel = true;
+    }
+  }
 }
